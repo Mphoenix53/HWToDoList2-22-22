f75b89352df490e2d1cd0a93167646adea68a95b
Mphoenix53 <michaelphoenix53mailbox@gmail.com>
2022-02-23 09:41:53 -0700

    added some stuff

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import ToDoList from './TodoList';
 function App() {
   return (
     <div className="App">
-      <ToDoList name='books' initialTodos={[{id:1, todo:'figure this out', complete:true}]}/>
-      <ToDoList name='articles'/>
+      <ToDoList name='books' initialTodos={[{id:1, todo:'garfield', completed:false}]}/>
+      <ToDoList name='articles' initialTodos={[{id:1, todo:'bat man', completed:true}]}/>
       <ToDoList name='magazines'/>
     </div>
   );
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,34 @@
 import {useState} from 'react'
 const TodoList = (props)=>{
-  const [todos, setTodos]= useState([{id:1, todo:'finish this'}])
+
+  const [todos, setTodos]= useState(props.initialTodo || [])
+  const [todo, setTodo] = useState('')
+  const renderTodos =() => {
+    return todos.map((x) => {
+      return (
+        <div key={x.id} style={{textDecoration: x.completed ? "line-thruogh" : ""}}>
+        <p>{x.todo}</p>
+        </div>
+      );
+    });
+  };
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    console.log(e)
+    console.log('todo: ', todo)
+    const newTodos = [...todos, {id:Math.random(), todo: todo, completed:false}]
+    setTodos(newTodos)
+  }
   return (
     <div>
       <h1>{props.name}</h1>
-      <div>{JSON.stringify(todos)}</div>
+      <form onSubmit={handleSubmit}>
+        <p>todo: </p>
+        <input value={todo} onChande={(e)=> {setTodo(e.target.value)} }/>
+        <button type='submit'>Enter</button>
+      </form>
+      <div>{renderTodos()}</div>
+      
     </div>
   );
 };
